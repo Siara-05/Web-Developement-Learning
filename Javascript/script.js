37489658cae3dc79b2f0fccce23ac3a0c45376e6
Siara-05 <siara240824@gamil.com>
2025-07-15 23:32:08 +0530

    Merge branch 'main' of https://github.com/Siara-05/Web-Developement-Learning

---
+++ Javascript/script.js
@@ -0,0 +1,23 @@
+// Start count from 0
+let count = 0;
+
+// Get the span element where count will be shown
+const countDisplay = document.getElementById("count");
+
+// Get the "Click Me!" button
+const clickBtn = document.getElementById("clickBtn");
+
+// Get the "Reset" button
+const resetBtn = document.getElementById("resetBtn");
+
+// When clickBtn is clicked, increase count by 1
+clickBtn.addEventListener("click", function () {
+  count++; // Increase count
+  countDisplay.textContent = count; // Show new count on screen
+});
+
+// When resetBtn is clicked, reset count to 0
+resetBtn.addEventListener("click", function () {
+  count = 0; // Reset count
+  countDisplay.textContent = count; // Update the screen
+});
