add923bff4c5da88d7c1a24cc36ed5eb85e70826
Siara-05 <siara240824@gamil.com>
2025-07-16 18:59:04 +0530

    js learning

---
+++ Javascript/Learning/Dom/scriot.js
@@ -0,0 +1,10 @@
+
+let h1 = document.createElement("h1");
+//  h1.textContent = "Hie, I am Siara";
+
+h1.style.color = "yellow";
+h1.style.backgroundColor = "black";
+
+ console.dir(h1);
+ 
+//  document.querySelector("div").prependpendChild(h1);
