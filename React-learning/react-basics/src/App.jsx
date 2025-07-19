bba86cedccca9f0e507493f140c947db8e2fe754
Siara-05 <siara240824@gamil.com>
2025-07-19 22:14:23 +0530

    increasing and decreasing a num by 10 by useState

--- React-learning/react-basics/src/App.jsx
+++ React-learning/react-basics/src/App.jsx
@@ -1,11 +1,53 @@
-import React from "react";
+// Using React to create a simple component that displays a value and changes it on button click
+
+// import React, { useState } from "react";
+
+// const App = () => {
+  
+//   const [a, setA] = useState(10)  
+  
+//   const changeA = () => {
+//     console.log("Button clicked");
+//     setA(60);
+//   }
+
+//   return (
+//     <div>
+//      <h1>Value of a is {a}</h1>
+//      <button onClick={changeA}>Chnage A</button>
+//     </div>
+//   )
+// }
+
+// export default App;
+
+
+        //  Increaasing and Decreasing a number using React
+
+
+
+import React, { useState } from 'react'
+
 
 const App = () => {
+
+const [num, setNum] = useState(0);
+
+ const increatment = () => {
+  setNum(num + 10);
+};
+
+ const decrement = () => {
+  setNum(num - 10);
+};
+
   return (
     <div>
-      <h1>Hello</h1>
-      <h1>Bonjour</h1>
-      </div>
-    );
-}
-export default App;
+      <h2>Number is {num} </h2>
+      <button onClick={increatment}>Increatment</button>
+      <button onClick= {decrement}>Decreatment</button>
+    </div>
+  );
+};
+
+export default App
