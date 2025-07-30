524994661bebf59cfd8566b5a23bb090616fcf0b
Siara-05 <siara240824@gmail.com>
2025-07-30 22:24:16 +0530

    useRef hook

--- React-learning/UseRef Hook/useRef hook/src/App.jsx
+++ React-learning/UseRef Hook/useRef hook/src/App.jsx
@@ -16,14 +16,13 @@ function App() {
 
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-r from-blue-100 to-purple-200">
-      <h1 className="text-3xl font-bold text-gray-800 mb-6">
-        React Counter with useRef
+      <h1 className="mb-6 text-3xl font-bold text-gray-800">
+         React Counter with useRef
       </h1>
 
       <button
         onClick={handleIncrement}
-        className="bg-blue-600 hover:bg-blue-700 text-white px-6 py-3 rounded-lg shadow-md transition-all duration-300"
-      >
+        className="px-6 py-3 text-white transition-all duration-300 bg-blue-600 rounded-lg shadow-md hover:bg-blue-700">
         Increment
       </button>
 
