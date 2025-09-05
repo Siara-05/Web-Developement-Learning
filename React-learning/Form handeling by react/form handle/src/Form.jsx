fd37d60d4fbcce7e9ff27ba966a9f183e73b1048
Siara-05 <siara240824@gmail.com>
2025-09-06 00:17:47 +0530

    added css and updated

--- React-learning/Form handeling by react/form handle/src/Form.jsx
+++ React-learning/Form handeling by react/form handle/src/Form.jsx
@@ -1,20 +1,59 @@
-import React, { useReducer } from 'react'
+import React, { useReducer } from 'react';
 import './App.css';
 
-function Form() {
-    const initialState = { email: ',password:'}
-    const [state, dispatch] = useReducer(useReducer,initialState)
-  return (
-    <div>
-        <form >
-            <input type="email" onChange={(e)=>{
-                dispatch({type: 'Email',payload: e.target.value})
-            }} />
-
-        </form>
-
-    </div>
-  )
+// Reducer function
+function reducer(state, action) {
+  switch (action.type) {
+    case 'SET_EMAIL':
+      return { ...state, email: action.payload };
+    case 'SET_PASS':
+      return { ...state, password: action.payload };
+    default:
+      return state;
+  }
 }
 
-export default Form
+function Form() {
+  const initialState = { email: '', password: '' };
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Logged in with\nEmail: ${state.email}\nPassword: ${state.password}`);
+  };
+
+  return (
+    <div className="form-container">
+      <form className="login-form" onSubmit={handleSubmit}>
+        <h2>Welcome Back 👋</h2>
+        <p>Please login to continue</p>
+
+        <div className="input-group">
+          <label>Email</label>
+          <input 
+            type="email" 
+            placeholder="Enter your email"
+            value={state.email}
+            onChange={(e) => dispatch({ type: 'SET_EMAIL', payload: e.target.value })}
+            required
+          />
+        </div>
+
+        <div className="input-group">
+          <label>Password</label>
+          <input 
+            type="password" 
+            placeholder="Enter your password"
+            value={state.password}
+            onChange={(e) => dispatch({ type: 'SET_PASS', payload: e.target.value })}
+            required
+          />
+        </div>
+
+        <button type="submit" className="btn">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Form;
