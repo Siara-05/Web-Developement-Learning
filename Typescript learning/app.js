3bab53327e077e0bf6d950317dbb68491fe355fb
Siara-05 <siara240824@gmail.com>
2025-09-08 00:16:19 +0530

    compiling ts and js

---
+++ Typescript learning/app.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var a = 12;
+function abcd() {
+    console.log(a);
+}
+let start = 16;
+const alphabet = "hi";
+//# sourceMappingURL=app.js.map
---
+++ Typescript learning/app.ts
@@ -0,0 +1,8 @@
+ var a =12;
+
+ function abcd(){
+    console.log(a);
+ }
+
+ let start = 16;
+ const alphabet = "hi";
