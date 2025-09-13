e32d33058cc799aaa7e7c68bf446d2e0c9dd51fe
Siara-05 <siara240824@gmail.com>
2025-09-14 00:54:23 +0530

    references

--- Typescript learning/TypeScript Types Basics/Types.ts
+++ Typescript learning/TypeScript Types Basics/Types.ts
@@ -1,10 +1,34 @@
 
-// Array Types: 
+//  BASIC TYPES
+
+// Primritives and reference
+
+// Any values under these brackets "[], {}, () are references and if no bracket they are primitives "
+
+//  expample
+
+// 12  - primitive
+// 4 - primitive
+// null - primitive
+
+"string"
+{}
+[]
+// these three are references 
+
+// 1) Primitive types (number, string , boolean)
+
+// 2) Array Types: 
 
 let numbers: number[] = [1, 2, 3];
 let strings: Array<string> = ["a", "b", "c"];
 
-
-// Tuple Types:
+// 3) Tuple Types:
 
 let tuple: [string, number] = ["hello", 42];
+
+// 4) Enums 
+// 5) Other, unknown, void, undefined, null , never
+
+
+
