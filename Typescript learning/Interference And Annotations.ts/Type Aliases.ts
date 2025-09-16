934730616e067f35bd0228726ac24f5d0fdc4c0c
Siara-05 <siara240824@gmail.com>
2025-09-16 21:46:48 +0530

    Aliases

--- Typescript learning/Interference And Annotations.ts/Type Aliases.ts
+++ Typescript learning/Interference And Annotations.ts/Type Aliases.ts
@@ -1,7 +1,7 @@
 
 // Type Aliases are similar to Interfaces, but they are more flexible.
 // They can represent not just object shapes, but also unions, primitives, functions, etc.
-// They merge the primitive types in one and make their own new type
+// They merge the primitive types in one and make their own new type of primitive
 
 type sankhya = number | null;
 let ab: sankhya;
